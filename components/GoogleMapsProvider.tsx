54f5070e450c8650e3232f8e5cd55eac40e34f4f
hanumtyas19 <hanumtyas2004@gmail.com>
2025-06-05 15:20:44 +0700

    Update Api Key Gmaps

--- components/GoogleMapsProvider.tsx
+++ components/GoogleMapsProvider.tsx
@@ -3,7 +3,7 @@ import { LoadScript } from "@react-google-maps/api";
 
 export default function GoogleMapsProvider({ children }: { children: React.ReactNode }) {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyCPcc7NZeArZrA2X79nClEcYyv-hGY4iC8">
+      <LoadScript googleMapsApiKey="AIzaSyB3csBUDnxM-oYSH0t-kRxiBuX5SuPdvrU">
       {children}
     </LoadScript>
   );
